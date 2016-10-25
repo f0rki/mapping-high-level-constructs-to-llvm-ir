81b67b22322deb8e2964b6433d40ac20a59bc3c4
Michael Rodler <contact@f0rki.at>
2016-10-25 14:55:51 +0200

    updated convert to support inline formatting and writing SUMMARY.md

--- original_post/convert.py
+++ original_post/convert.py
@@ -80,15 +80,19 @@ class Converter:
 
     def __enter__(self):
         self.curof = open(os.path.join(self.outdir, "README.md"), "w")
+        self.summaryf = open(os.path.join(self.outdir, "SUMMARY.md"), "w")
         self.inp = LineBuffer(self.infile)
         return self
 
     def __exit__(self, type, value, traceback):
         self.curof.close()
+        self.summaryf.close()
         self.inp.destroy()
         return False
 
     def convert_to_md(self):
+        self.summaryf.write("# Summary\n\n")
+
         for i, line in self.inp:
             self.current_line = i
             if line.startswith("!"):
@@ -104,6 +108,14 @@ class Converter:
 
     def process_line(self, line):
         # TODO: regex replace
+        line = re.sub(r"{c:(.+)}", r"`\1`", line)
+        line = re.sub(r"{b:(.+)}", r"**\1**", line)
+        line = re.sub(r"{i:(.+)}", r"*\1*", line)
+        line = re.sub(r"{link:([^|]+)\|([^}]+)}",
+                      r"[\2](\1)", line)
+        line = re.sub(r"{mail:([^|]+)\|([^}]+)}",
+                      r"[\2](mailto:\1)", line)
+        line = line.replace("#", "-")
         self.curof.write(line)
 
     def handle_todo(self, line):
@@ -163,23 +175,35 @@ class Converter:
     def handle_cmd(self, cmd, args):
         if cmd == "!chapter":
             self.info("processing chapter", args)
-            args = args.replace("/", "+")
-            self.current_chapter = space_to_dash(args.strip())
+            chaptername = args.strip()
+            self.current_chapter = (space_to_dash(chaptername.strip())
+                                    .replace("/", "+"))
             self.current_section = ""
             self.switch_file("README.md", self.current_chapter)
             self.current_listing = 0
-            self.curof.write("= {} =".format(self.current_chapter))
+            self.curof.write("# {}\n".format(args))
+
+            self.summaryf.write("* [{}]({}/README.md)\n"
+                                .format(chaptername,
+                                        self.current_chapter))
         elif cmd == "!section":
             self.info("processing section", args, "in", self.current_chapter)
-            args = args.replace("/", "+")
-            self.current_section = space_to_dash(args.strip())
+            sectionname = args.strip()
+            self.current_section = (space_to_dash(sectionname)
+                                    .replace("/", "+"))
             self.switch_file(self.current_section, self.current_chapter)
-            self.curof.write("== {} ==".format(self.current_section))
+            self.curof.write("## {}\n".format(sectionname))
+            self.curof.write("\n\n")
+
+            self.summaryf.write("   * [{}]({}/{}.md)"
+                                .format(sectionname,
+                                        self.current_chapter,
+                                        self.current_section))
         elif cmd == "!subsection":
-            self.curof.write("=== {} ===".format(args))
+            self.curof.write("### {}\n".format(args))
         else:
             self.warn("unknown command '{}'".format(cmd))
-            self.curof.write("`{} {}`".format(cmd, args))
+            # self.curof.write("`{} {}`".format(cmd, args))
 
     def warn(self, *args):
         s = " ".join(map(str, args))
