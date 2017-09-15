7390addac9c0d4ed1ec68b67d14e63b38b6bf122
Michael Rodler <michael.rodler@paluno.uni-due.de>
2017-09-15 14:22:43 +0200

    removed unnecessary comments in listing

--- advanced-constructs/listings/lambda_func_1.cpp
+++ advanced-constructs/listings/lambda_func_1.cpp
@@ -1,10 +1,8 @@
 extern int integer_parse();
 
-/// [show]
 int foo(int a, int b)
 {
   int c = integer_parse();
   auto function = [a, b, c](int x) { return (a + b - c) * x; };
   return function(10);
 }
-/// [show]
